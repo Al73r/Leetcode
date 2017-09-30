5f0ee0898d48123b6a68e4fe2f3452e620f6b18c
StayJF <fyzh01@163.com>
2017-09-30 17:08:07 +0800

    2017/9/30

--- 37. Sudoku Solver/sudokuSolver.cpp
+++ 37. Sudoku Solver/sudokuSolver.cpp
@@ -26,41 +26,69 @@ public:
                 n[i][j] = 0;
             }
         }
+        infer(board, n, p);
+        
+    }
+private:
+    void infer(vector<vector<char>>& board, vector<vector<int>>& n, vector<vector<vector<bool>>>& p){
         bool update = true;
         while(update){
             update = false;
-            for(int i=0; i<9; i++){
-                for(int j=0; j<9; j++){
-                    if(n[i][j]!=1)
-                        continue;
-                    update = true;
-                    for(int num=0; num<9; num++){
-                        if(!p[i][j][num])
-                            continue;
-                        board[i][j] = num + '0' + 1;
-                        for(int k=0; k<9; k++){
-                            if(p[i][k][num]){
-                                n[i][k]--;
-                                p[i][k][num] = false;
-                            }
-                            if(p[k][j][num]){
-                                n[k][j]--;
-                                p[k][j][num] = false;
-                            }
-                            int x = i / 3 * 3 + k / 3;
-                            int y = j / 3 * 3 + k % 3;
-                            if(p[x][y][num]){
-                                n[x][y]--;
-                                p[x][y][num] = false;
-                            }
-                        }
+            int i, j, num;
+            for(i=0; i<9; i++){
+                for(j=0; j<9; j++){
+                    if(n[i][j]==1){
+                        update = true;
                         break;
                     }
-                    break;
                 }
                 if(update) break;
             }
+            if(!update) break;
+            for(num=0; num<9; num++){
+                if(p[i][j][num])
+                    break;
+            }
+            board[i][j] = num + '0' + 1;
+            for(int k=0; k<9; k++){
+                if(p[i][k][num]){
+                    n[i][k]--;
+                    p[i][k][num] = false;
+                }
+                if(p[k][j][num]){
+                    n[k][j]--;
+                    p[k][j][num] = false;
+                }
+                int x = i / 3 * 3 + k / 3;
+                int y = j / 3 * 3 + k % 3;
+                if(p[x][y][num]){
+                    n[x][y]--;
+                    p[x][y][num] = false;
+                }
+            }
         }
-        
+    }
+    bool dfs(vector<vector<char>>& board, vector<vector<int>>& n, vector<vector<vector<bool>>>& p){
+        int i, j, num;
+        bool update = false;
+        for(i=0; i<9; i++){
+            for(j=0; j<9; j++){
+                if(n[i][j]==2){
+                    update = true;
+                    break;
+                }
+            }
+            if(update) break;
+        }
+        if(n[i][j]!=2)
+            return false;
+        for(num=0; num<9; num++){
+            if(!p[i][j][num])
+                continue;
+            vector<vector<char>> board2(board);
+            vector<vector<int>> n2(n);
+            vector<vector<vector<bool>>> p;
+            //dfs(b);
+        }
     }
 };
