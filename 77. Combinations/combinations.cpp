295fca22890558500fda75115efc6dc0e48ccee5
StayJF <fyzh01@163.com>
2018-02-01 01:13:23 -0800

    Feb 1

--- 77. Combinations/combinations.cpp
+++ 77. Combinations/combinations.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
-        
+        vector<int> path;
+        vector<vector<int>> result;
+        helper(n, k, 0, path, result);
+        return result;
+    }
+
+private: 
+    void helper(int n, int k, int last, vector<int> &path, vector<vector<int>> &result) {
+        if(k==0) {
+            result.push_back(path);
+            return;
+        }
+        for(int i=last+1; i<=n-k+1; i++){
+            path.push_back(i);
+            helper(n, k-1, i, path, result);
+            path.pop_back();
+        }
     }
 };
+
+
